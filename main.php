71b1c28baaf8d905462a23a7bc16ab9d3659eabf
yakitama <pikaiv@gmail.com>
2019-01-03 14:17:00 +0900

    順位変化がない場合、トゥートをスキップする機能を追加

--- main.php
+++ main.php
@@ -70,25 +70,33 @@ if ( $lasttags_log === FALSE ) {
 	// 前回報告したトレンドタグの一覧が見当たらない場合は何も報告していないとして処理
 	$lasttags_log = array();
 }
-$lasttags_array = array();
+$lasttags_array_score = array();
 foreach ( $lasttags_log as $lasttags_log_oneline ) {
-	list($last_tag_text, $last_tag_score) = explode(",", rtrim($lasttags_log_oneline));
-	$lasttags_array[$last_tag_text] = $last_tag_score;
+	list($last_tag_text, $last_tag_score, $last_tag_rank) = explode(",", rtrim($lasttags_log_oneline));
+	$lasttags_array_score[$last_tag_text] = $last_tag_score;
+
+	// ログバージョンが古い場合、last_tag_rank がへんな値になってる可能性があるので、ガード
+	if ( !isset($last_tag_rank) || !is_int($last_tag_rank) || ($last_tag_rank <= 0) ) {
+		$last_tag_rank = 0;
+	}
+	$lasttags_array_rank[$last_tag_text] = $last_tag_rank;
 }
 
 // トレンドタグ報告文を作成する
-$report_tags = array();
-$save_tags = array();
+$report_tags = array();			// トゥート文を保存する配列
+$save_tags = array();			// 内部データとして保存する配列
+$current_rank = 1;				// ランク情報計算用
+$rank_diff = FALSE;				// 順位変化ありフラグ
 foreach ( $trend_tags_result['score'] as $current_tag_text => $current_tag_score ) {
 	if ( count($report_tags) < REPORT_TAGS_NUM ) {
 		$report_tag = '#️⃣ '.$current_tag_text.' ['.$current_tag_score.']';
 		// トレンドタグが前回報告した一覧にも存在するかチェック
-		if ( array_key_exists($current_tag_text, $lasttags_array) === TRUE ) {
+		if ( array_key_exists($current_tag_text, $lasttags_array_score) === TRUE ) {
 			// 存在する場合はこちら
 			// スコアが上昇したか下降したかチェック
 			// スコアは 0.01 以上の変化が見られた場合にだけ上昇/下降した、と判断する
 			// スコアは float で得られるので、「一致」した場合を == みたいなやつで判断できないので。
-			$score_diff = $current_tag_score - $lasttags_array[$current_tag_text];
+			$score_diff = $current_tag_score - $lasttags_array_score[$current_tag_text];
 			if ( $score_diff > 0.01 ) {
 				$score_movement = '↗️ '.$score_diff;
 			}
@@ -98,21 +106,30 @@ foreach ( $trend_tags_result['score'] as $current_tag_text => $current_tag_score
 			else {
 				$score_movement = '➡️ '.$score_diff;
 			}
+
+			// 前回一覧に存在するなら、ランクの変化をチェック
+			if ( $lasttags_array_rank[$current_tag_text] != $current_rank ) {
+				$rank_diff = TRUE;
+			}
 		}
 		else {
 			// 存在しない場合はこちら
 			$score_movement = 'NEW';
+			// 新しいタグがトレンドに新規掲載になったので、トゥート必要
+			$rank_diff = TRUE;
 		}
 		$report_tag .= '[ '.$score_movement.' ]';
 		$report_tags[] = $report_tag;
 	}
 
 	// 保存用のデータを作成する
-	$save_tags[] = $current_tag_text.','.$current_tag_score;
+	$save_tags[] = $current_tag_text.','.$current_tag_score.','.$current_rank;
+	$current_rank++;
 }
 
-// 作成したトレンドタグ報告文に、1 つ以上のハッシュタグが含まれる場合、トゥートする
-if ( count($report_tags) > 0) {
+// 作成したトレンドタグ報告文に、1 つ以上のハッシュタグが含まれる場合、かつ
+// 順位変化がある場合、トゥートする
+if ( (count($report_tags) > 0) && ($rank_diff === TRUE) ) {
 	require_once("MastodonClient/MastodonClient.php");
 	$mc = new MastodonClient();
 	$mc->init();
